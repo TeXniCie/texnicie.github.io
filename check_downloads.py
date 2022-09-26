2ed79762cd722280009339db71a18eb1e4c79d27
Vincent Kuhlmann <vincent.kuhlmann@hotmail.com>
2022-09-26 20:06:37 +0200

    Fix missing downloads

---
+++ check_downloads.py
@@ -0,0 +1,43 @@
+from __future__ import annotations
+
+from pathlib import Path, PurePosixPath
+import shutil
+import re
+import itertools
+
+# Copyright (c) 2022 Vincent Kuhlmann
+
+ROOT_DIR = Path.cwd()
+CSS_DIR = ROOT_DIR / "css"
+FONTS_DIR = ROOT_DIR / "fonts"
+
+PAGES_DIR = ROOT_DIR / "src" / "pages"
+DEST_DIR = ROOT_DIR / "build"
+
+downloads_source = Path.cwd().parent / "texnicie-www-downloads"
+
+assert downloads_source.exists()
+
+def main():
+    regex = re.compile(r'"(?P<downloadPath>/downloads/[^"]+)')
+    for a in PAGES_DIR.glob("**/*.html"):
+        print(f"In {PurePosixPath(a.relative_to(PAGES_DIR))}")
+
+        with a.open("r", encoding="utf-8") as f:
+            contents = f.read()
+
+        for m in regex.finditer(contents):
+            online_path = PurePosixPath(m.group("downloadPath"))
+            local_path = downloads_source / online_path.relative_to("/downloads")
+
+            if not local_path.exists():
+                print(f"    {online_path}")
+        
+        #regex.match(a)
+
+
+if __name__ == "__main__":
+    main()
+
+
+
